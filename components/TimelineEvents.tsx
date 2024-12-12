6344a1b2b6244bf6f6ed5abd0178c202200e603f
Bulata Damian <inamida16@gmail.com>
2024-12-12 12:41:31 +0200

    error fix on vercel deploy

--- components/TimelineEvents.tsx
+++ components/TimelineEvents.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useMemo, useRef } from 'react';
+import React, { useMemo } from 'react';
 import { Clock, MapPin } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
