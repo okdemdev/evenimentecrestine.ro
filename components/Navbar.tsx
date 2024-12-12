4021b37778d55bd2ec6d0bc1f4ea02931db51405
Bulata Damian <inamida16@gmail.com>
2024-12-12 12:45:07 +0200

    change navbar icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { BookOpen } from 'lucide-react';
+import { Church } from 'lucide-react';
 import Link from 'next/link';
 
 const Navbar = () => {
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <nav className="container flex h-14 items-center justify-center px-4 max-w-[1400px] mx-auto">
         <Link href="/" className="flex items-center gap-1.5">
-          <BookOpen className="h-5 w-5 md:h-6 md:w-6 text-[#6a7bff]" />
+          <Church className="h-5 w-5 md:h-6 md:w-6 text-[#6a7bff]" />
           <span className="text-sm md:text-lg font-semibold whitespace-nowrap">
             Evenimente Creștine
           </span>
